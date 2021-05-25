5bbf49164f25f300cdb1ff8e63abef0b20f19c14
Kuntal Das <25ckuntaldas@gmail.com>
2021-05-25 19:26:25 +0000

    minor fixes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,15 +5,15 @@ const MovieCard = (props) => {
   return(
     <div className="card">
       <img 
-        className="card__img" 
+        className="card-img" 
         src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${props.poster_path}`}
         alt={`${props.title}_poster`}
       />
-      <div className="card--content">
-        <h3 classname="card--title">{props.title}</h3>
+      <div className="card-content">
+        <h2 className="card-title">{props.title}</h2>
         <p><small>Release Date : {props.release_date}</small></p>
         <p><small>Rating : {props.vote_average}</small></p>
-        <p className="card--desc">{props.overview}</p>
+        <p className="card-desc">{props.overview}</p>
       </div>
     </div>
   )
